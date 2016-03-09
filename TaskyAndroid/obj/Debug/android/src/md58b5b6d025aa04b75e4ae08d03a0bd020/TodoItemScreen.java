861c8087100926e72de40358ba460b7a1b6a7018
GayathriKaki <ghani319@gmail.com>
2016-03-09 11:08:47 +1100

    Notes app

---
+++ TaskyAndroid/obj/Debug/android/src/md58b5b6d025aa04b75e4ae08d03a0bd020/TodoItemScreen.java
@@ -0,0 +1,46 @@
+package md58b5b6d025aa04b75e4ae08d03a0bd020;
+
+
+public class TodoItemScreen
+	extends android.app.Activity
+	implements
+		mono.android.IGCUserPeer
+{
+	static final String __md_methods;
+	static {
+		__md_methods = 
+			"n_onCreate:(Landroid/os/Bundle;)V:GetOnCreate_Landroid_os_Bundle_Handler\n" +
+			"";
+		mono.android.Runtime.register ("TaskyAndroid.Screens.TodoItemScreen, TaskyAndroid, Version=1.0.5847.39930, Culture=neutral, PublicKeyToken=null", TodoItemScreen.class, __md_methods);
+	}
+
+
+	public TodoItemScreen () throws java.lang.Throwable
+	{
+		super ();
+		if (getClass () == TodoItemScreen.class)
+			mono.android.TypeManager.Activate ("TaskyAndroid.Screens.TodoItemScreen, TaskyAndroid, Version=1.0.5847.39930, Culture=neutral, PublicKeyToken=null", "", this, new java.lang.Object[] {  });
+	}
+
+
+	public void onCreate (android.os.Bundle p0)
+	{
+		n_onCreate (p0);
+	}
+
+	private native void n_onCreate (android.os.Bundle p0);
+
+	java.util.ArrayList refList;
+	public void monodroidAddReference (java.lang.Object obj)
+	{
+		if (refList == null)
+			refList = new java.util.ArrayList ();
+		refList.add (obj);
+	}
+
+	public void monodroidClearReferences ()
+	{
+		if (refList != null)
+			refList.clear ();
+	}
+}
